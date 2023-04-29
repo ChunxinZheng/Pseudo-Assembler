f3a60a356b427bcf45fcafe385de4e00d65edead
ChunxinZheng <115291818+ChunxinZheng@users.noreply.github.com>
2023-04-28 21:15:05 -0400

    Update Assembler.rkt

--- Assembler.rkt
+++ Assembler.rkt
@@ -2,6 +2,38 @@
 
 ;; AUTHORS: Lex Stapleton, Chunxin Zheng
 
+;;    Basic Information 
+;; ---------------------------------------------------------------------
+
+;; This is an assembler that converts a pseudocode PRIMPL to its assembler language A-PRIMPL.
+;; Both languages were created by the University of Waterloo CS 146 instructor team.
+;; More detailed information, including the grammar for both languages, is included in README.
+;; The PRIMPL Simulator (Simulator.rkt) was provided by the University of Waterloo
+;; CS 146 instructor team. 
+
+
+;;    User Guide 
+;; ---------------------------------------------------------------------
+
+;; 
+
+
+
+
+;;    Assemble (Main Function) 
+;; ---------------------------------------------------------------------
+
+(define (primpl-assemble aprimpl)
+  (define scanned (first-scan aprimpl empty 0 empty))
+  (define prog-with-psymbols (result-prog scanned))
+  (define psymbol-list (result-table scanned))
+  (define psymbol-table (generate-psymbol-table psymbol-list))
+  (translate prog-with-psymbols psymbol-table empty))
+
+
+;;    Error Handling
+;; ---------------------------------------------------------------------
+
 (define ((base-error msg) extra) (error (format "~a: ~a" msg extra)))
 
 (define circular-reference-error  (base-error "circular reference error"))
@@ -9,19 +41,30 @@
 (define duplicate-reference-error (base-error "duplicate symbol"))
 (define incorrect-usage-error (base-error "incorrect symbol usage"))
 
+
+;;    Basic Types of Special Pseudo-instructions
+;; ---------------------------------------------------------------------
+
 (struct psymbol (type name val) #:transparent)
 (define (make-label name loc) (psymbol 'label name loc))
 (define (make-const name val) (psymbol 'const name val))
 (define (make-data  name loc) (psymbol 'data  name loc))
 
 
-;;    FIND ALL SYMBOLS && GENERATE ADDITIONAL INSTRUCTIONS
+;;    First Scan
 ;; ---------------------------------------------------------------------
 
 (struct result (prog table)  #:transparent #:mutable)
 
+;; An instruction (Ins) is (listof Sym)
 
-;; (first-scan aprimpl empty 0 empty), reversed
+;; first-scan: (listof Ins) (listof Ins) Int (listof psymbol) -> result
+
+;; [first-scan] scans through the given code for the first time,
+;; collect special pseudo-instructions ([label], [const], and [data]) to a table,
+;; and modified the given code
+
+;; (first-scan aprimpl empty 0 empty), produced new code is reversed
 (define (first-scan lst acc count table)
   (cond [(empty? lst) (result acc table)]
         [else
@@ -31,54 +74,47 @@
             (first-scan (rest lst) acc count (cons (make-const psymbol val) table))]
            [`(label ,psymbol)
             (first-scan (rest lst) acc count (cons (make-label psymbol count) table))]
-;           [`(lit ,psymbol)
-;            (first-scan (rest lst) (cons psymbol acc) count table)]
            [`(data ,psymbol (,nat ,val))
-;;            (define c (cond [(symbol? val) val] [else count]))
             (first-scan (rest lst) (cons (first lst) acc)
                         (+ nat count) (cons (make-data psymbol count) table))]
            [`(data ,psymbol ...)
-;            (display psymbol)
-;            (display (first psymbol))
-;;            (define c (cond [(symbol? (second psymbol)) (second psymbol)] [else count]))
             (first-scan (rest lst)
                         (cons (first lst) acc)
-;                        (append (foldl (lambda (x l) (cons (list x) l))
-;                                       empty (rest (rest (first lst)))) acc)
                         (+ (- (length (first lst)) 2) count)
                         (cons (make-data (first psymbol) count) table))]
            [else (first-scan (rest lst) (cons (first lst) acc) (+ 1 count) table)])]))
 
 
 
-;;    GENERATE SYMBOL TABLE
+;;    Generate Symbol Table
 ;; ---------------------------------------------------------------------
+
 (define (make-psymbol-hash-table psymbols)
   (define (h psymbols table)
-    (define (name) (psymbol-name (car psymbols))) ;; make name a thunk to stop it from evaluating when psybols is empty
+    (define (name) (psymbol-name (car psymbols)))
+    ;; make name a thunk to stop it from evaluating when psymbols is empty
     (cond
-     [(empty? psymbols) table]
-     [(not (hash-ref-key table (name) #f)) (h (cdr psymbols) (hash-set table (name) (car psymbols)))]
-     [else (duplicate-reference-error (format "found duplicate symbol ~a" name))]))
+      [(empty? psymbols) table]
+      [(not (hash-ref-key table (name) #f))
+       (h (cdr psymbols) (hash-set table (name) (car psymbols)))]
+      [else (duplicate-reference-error (format "found duplicate symbol ~a" name))]))
   (h psymbols (make-immutable-hash)))
 
 (define (generate-psymbol-table psymbols)
   (define (h sym-table env cur stack defs is_base)
-    ;;(printf "sym:~a\n" sym-table)
-    ;;(printf "env:~a\n" env)
-    ;;(printf "cur:~a\n" cur)
-    ;;(printf "def:~a\n--------------------\n" defs)
     (cond
       [(not (eq? 'undef cur))
        (define type (psymbol-type cur))
        (define name (psymbol-name cur))
        (define val  (psymbol-val  cur))
-       (if (lit? val) (h (hash-set sym-table name (psymbol type name val)) env 'undef stack defs is_base)
+       (if (lit? val)
+           (h (hash-set sym-table name (psymbol type name val)) env 'undef stack defs is_base)
            (let
                ([stored-val (hash-ref sym-table val #f)]
                 [traced-val (hash-ref env       val #f)])
              (cond
-               [stored-val (h (hash-set sym-table name (psymbol type name (psymbol-val stored-val))) env 'undef stack defs #t)]
+               [stored-val (h (hash-set sym-table name (psymbol type name (psymbol-val stored-val)))
+                              env 'undef stack defs #t)]
                [traced-val
                 (define traced-env (hash-remove env name))
                 (h sym-table traced-env traced-val (cons (list env cur) stack) defs #f)]
@@ -87,12 +123,11 @@
                     (undefined-reference-error val)
                     (circular-reference-error val))]
                )))]
-      [(not (empty? stack )) (h sym-table (first (first stack)) (second (first stack)) (cdr stack) defs #t)]
+      [(not (empty? stack )) (h sym-table (first (first stack))
+                                (second (first stack)) (cdr stack) defs #t)]
       [(not (empty? defs  )) (h sym-table env (first defs) stack (rest defs) #t)]
       [else sym-table]
-      )
-    )
-
+      ))
   (h (make-immutable-hash)
      (make-psymbol-hash-table psymbols)
      'undef
@@ -101,8 +136,11 @@
      #t))
   
 
-;;    RESOLVING PARAMETERS 
+;;    Resolving Parameters 
 ;; ---------------------------------------------------------------------
+
+
+;; ----- Type Checking Helpers -----
 
 (define (lit? l) (or (number? l) (boolean? l)))
 
@@ -110,8 +148,12 @@
 (define (data? l)  (and (psymbol? l) (symbol=? 'data  (psymbol-type l))))
 (define (const? l) (and (psymbol? l) (symbol=? 'const (psymbol-type l))))
 
+;; ----- Looking Up -----
 (define (lookup-entry table symbol)
   (hash-ref table symbol #f))
+
+
+;; ----- Resolving the First Opd in an Offset -----
 
 (define (resolve-index-first table symbol)
   (cond
@@ -120,13 +162,15 @@
      (unless psym (undefined-reference-error (format "psymbol ~a is undefined" symbol)))
      (define val (psymbol-val psym))
      (cond
-         [(const? psym) val]
-         [(data?  psym) val]
-         [else (incorrect-usage-error (format "cannot use ~a as the first value of an indexed access" symbol))])]
+       [(const? psym) val]
+       [(data?  psym) val]
+       [else (incorrect-usage-error
+              (format "cannot use ~a as the first value of an indexed access" symbol))])]
     [(lit? symbol) symbol]
-    [else symbol]));; remove this if we want to check this error
-    ;;[else (incorrect-usage-error (format "cannot use ~a as the first value of an indexed access" symbol))]))
+    [else symbol]))
 
+
+;; ----- Resolving the Second Opd in an Offset -----
 
 (define (resolve-index-second table symbol)
   (cond
@@ -135,13 +179,15 @@
      (unless psym (undefined-reference-error (format "psymbol ~a is undefined" symbol)))
      (define val (psymbol-val psym))
      (cond 
-         [(const? psym) (list val)]
-         [(data?  psym) (list val)]
-         [else (incorrect-usage-error (format "cannot use ~a as the second value of an indexed access" symbol))])]
+       [(const? psym) (list val)]
+       [(data?  psym) (list val)]
+       [else (incorrect-usage-error
+              (format "cannot use ~a as the second value of an indexed access" symbol))])]
     [(list? symbol) symbol]
-    [else symbol]));; remove this if we want to check this error
-    ;;[else (incorrect-usage-error (format "cannot use ~a as the second value of an indexed access" symbol))]))
+    [else symbol]))
 
+
+;; ----- Resolving [lit], [data] or [const] -----
 
 (define (resolve-lit-data-const table symbol)
   (cond
@@ -151,9 +197,10 @@
          (psymbol-val psym)
          (undefined-reference-error (format "psymbol ~a is undefined" symbol)))]
     [(lit? symbol) symbol]
-    [else symbol]));; remove this if we want to check this error
-    ;;[else (incorrect-usage-error (format "cannot use ~a as a lit/data/const" symbol))]))
+    [else symbol]))
 
+
+;; ----- Resolving [jump] or [branch] -----
 
 (define (resolve-jump-branch table symbol)
   (cond
@@ -162,14 +209,16 @@
      (unless psym (undefined-reference-error (format "psymbol ~a is undefined" symbol)))
      (define val (psymbol-val psym))
      (cond 
-         [(const? psym)       val ]
-         [(data?  psym) (list val)]
-         [(label? psym)       val ]
-         [else (incorrect-usage-error (format "cannot use ~a as a the destination of a jump/branchs" symbol))])]
+       [(const? psym)       val ]
+       [(data?  psym) (list val)]
+       [(label? psym)       val ]
+       [else (incorrect-usage-error
+              (format "cannot use ~a as a the destination of a jump/branchs" symbol))])]
     [(lit? symbol) symbol]
-    [else symbol]));; remove this if we want to check this error
-    ;;[else (incorrect-usage-error (format "cannot use ~a as a the destination of a jump/branch" symbol))]))
+    [else symbol]))
 
+
+;; ----- Resolving the Opd in Remaining Instructions -----
 
 (define (resolve-opd table symbol)
   (match symbol
@@ -182,17 +231,17 @@
      (unless psym (undefined-reference-error (format "psymbol ~a is undefined" symbol)))
      (define val (psymbol-val psym))
      (cond
-         [(const? psym)       val ]
-         [(data?  psym) (list val)]
-         [else (incorrect-usage-error (format "cannot use ~a as an opd" symbol))])]
-    [x symbol]));; remove this if we want to check this error
-    ;;[x (incorrect-usage-error (format "cannot use ~a as a the destination of a jump/branch" symbol))]))
+       [(const? psym)       val ]
+       [(data?  psym) (list val)]
+       [else (incorrect-usage-error (format "cannot use ~a as an opd" symbol))])]
+    [x symbol]))
+
+
+;; ----- Resolving the Dest in Remaining Instructions -----
 
 (define (resolve-dest table symbol)
   (match symbol
     [`(,fst ,snd)
-;     (display "matched")
-
      (list (resolve-index-first  table fst)
            (resolve-index-second table snd))]
     [(? symbol? symbol)
@@ -200,15 +249,18 @@
      (unless psym (undefined-reference-error (format "psymbol ~a is undefined" symbol)))
      (define val (psymbol-val psym))
      (cond
-         [(data? psym) (list val)]
-         [else (incorrect-usage-error (format "cannot use ~a as a dest" symbol))])]
-    [x symbol]));; remove this if we want to check this error
-    ;;[x (incorrect-usage-error (format "cannot use ~a as a the destination of a jump/branch" symbol))]))
+       [(data? psym) (list val)]
+       [else (incorrect-usage-error (format "cannot use ~a as a dest" symbol))])]
+    [x symbol]))
 
-;;    REPLACE PSYMBOLS
+;;    Final Replacement
 ;; ---------------------------------------------------------------------
 
+;; translate: (listof Ins) (listof psymbol) (listof Ins) -> (listof Ins)
+
 ; (translate prog table empty)
+;; [prog] is reversed, [translate] reverses it back
+
 (define (translate lst table acc)
   (cond [(empty? lst) acc]
         [else
@@ -228,33 +280,15 @@
            [`(data ,psymbol ,vals ...)
             (translate (rest lst) table
                        (append (foldr (lambda (x l) (cons (resolve-lit-data-const table x) l))
-                                       empty vals) acc))]
+                                      empty vals) acc))]
            [`(print-val ,opd) (translate (rest lst) table
-                       (cons (list 'print-val (resolve-opd table opd)) acc))]
+                                         (cons (list 'print-val (resolve-opd table opd)) acc))]
            [`(print-string ,str) (translate (rest lst) table (cons (first lst) acc))]
            [`(,fun ,dest ,opds ...)
             (translate (rest lst) table
                        (cons (append (list fun (resolve-dest table dest))
-                                           (map (lambda (opd) (resolve-opd table opd)) opds)) acc))]
+                                     (map (lambda (opd) (resolve-opd table opd)) opds)) acc))]
            ['(halt) (translate (rest lst) table (cons 0 acc))]
            [else (translate (rest lst) table (cons (first lst) acc))])]))
 
 
-
-;;    ASSEMBLE 
-;; ---------------------------------------------------------------------
-
-(define (primpl-assemble aprimpl)
-  (define scanned (first-scan aprimpl empty 0 empty))
-  (define prog-with-psymbols (result-prog scanned))
-  (define psymbol-list (result-table scanned))
-  ;;(display prog-with-psymbols)
-  ;;(newline)
-
-  ;(display psymbol-list)
-  ;(newline)
-  (define psymbol-table (generate-psymbol-table psymbol-list))
-  ;(display psymbol-table)
-  ;(newline)
-
-  (translate prog-with-psymbols psymbol-table empty))
